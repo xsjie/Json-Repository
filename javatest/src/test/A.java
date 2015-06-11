168730cf27c54683534c348071ddd7f6450174a1
xsjie <Li Gan@Lenovo-PC>
2015-06-11 15:30:56 +0800

    测试

---
+++ javatest/src/test/A.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @ClassName: A
+ * @Description: TODO
+ * @author xiongshengjie 382202220@qq.com
+ * @date 2015年6月10日 上午10:43:33
+ *
+ */
+public class A {
+
+	/**
+	 * 
+	 */
+	public A()
+	{
+		System.out.println("A be construct");
+	}
+	public void testA1()
+	{
+		System.out.println("test1");
+	}
+	public void testA2()
+	{
+		System.out.println("test2");
+	}
+}
---
+++ javatest/src/test/B.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @ClassName: B
+ * @Description: TODO
+ * @author xiongshengjie 382202220@qq.com
+ * @date 2015年6月10日 上午10:45:05
+ *
+ */
+public abstract class B extends A {
+
+	public abstract void testB1() ;
+}
